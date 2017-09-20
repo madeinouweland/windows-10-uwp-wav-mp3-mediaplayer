d80d2f1d25b8e935bce74282c03cd61ceacd4718
madeinouweland <lovanden@outlook.com>
2017-09-20 11:28:48 +0200

    make it so!

---
+++ SoundInUWP/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+
+namespace SoundInUWP
+{
+    public sealed partial class MainPage
+    {
+        private MediaPlayer _mediaPlayer = new MediaPlayer();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonMp3Click(object sender, RoutedEventArgs e)
+        {
+            Play("ms-appx:///closure.mp3");
+        }
+
+        private void OnButtonWavClick(object sender, RoutedEventArgs e)
+        {
+            Play("ms-appx:///matrix.wav");
+        }
+
+        private void Play(string fileName)
+        {
+            _mediaPlayer.Pause();
+            _mediaPlayer.Source = MediaSource.CreateFromUri(new Uri($"{fileName}", UriKind.RelativeOrAbsolute));
+            _mediaPlayer.Play();
+
+            AudioInfo.Text = $"Playing {fileName}";
+        }
+    }
+}
